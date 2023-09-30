7119bdb45c890a2decb59b1419a4efc271da2cb1
Yui-Uraoka <yui901116@gmail.com>
2023-09-30 12:38:25 +0900

    added search function on admin panel

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -18,6 +18,31 @@ class CategoriesController extends Controller
         $this->post = $post;
     }
 
+    private function uncategorizedCount() {
+        $uncategorized_count = 0;
+        $all_posts = $this->post->all();
+        foreach($all_posts as $post) {
+            if($post->categoryPost->count()==0) {
+                // $uncategorized_count[] = [data] *save and push data in an empty array variable
+                $uncategorized_count++; 
+                // this is to count and add 1 on every instance when IF-condition is satisfied
+                // $uncategorized_count = $uncategorized_count + 1;
+                /**
+                 * $uncategorized_count = 0
+                 * itr #1, post id = 1 (no category)
+                 * $uncategorized_count = 0 + 1     = 1
+                 * itr #2, post id = 2 (category)
+                 *  $uncategorized_count = 1 + 0    = 1
+                 * itr #3, post id = 3 (no category)
+                 * $uncategorized_count = 1 + 1     = 2
+                 */
+            }
+        }
+
+        return $uncategorized_count;
+
+    }
+
     public function index() {
         $all_categories = $this->category->orderBy('updated_at', 'desc')->paginate(5);
 
